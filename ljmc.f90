ffcc3625a9c9a525bdf1cef3b1b1370a286b93b4
Ionut GEORGESCU <ionut.georgescu@uci.edu>
2010-06-28 11:58:59 -0700

    stupid typo

--- ljmc.f90
+++ ljmc.f90
@@ -1,13 +1,13 @@
 program ljmc
-	integer, parameter :: natom = 13
-	real*8 :: q0(3,natom), fx(3,natom), y(1+21*natom), meff(3,3,natom), sqrtmeff(3,3,natom)
-	real*8, parameter :: imass = 2.0, taui = 1.0e-6, bl=40.0, atol=1.0e-4, rc=10.0
-	real*8 :: lnp, w, enrg, taumax
+        integer, parameter :: natom = 13
+        real*8 :: q0(3,natom), fx(3,natom), y(1+21*natom), invmeff(3,3,natom), sqrtmeff(3,3,natom)
+        real*8, parameter :: imass = 2.0, taui = 1.0e-6, bl=40.0, atol=1.0e-4, rc=10.0
+        real*8 :: lnp, w, enrg, taumax
 
-	open(20, file='Ne13.dat')
-	read(20, *) (q0(1,i), q0(2,i), q0(3,i),i=1,natom)	
-	close(20)
+        open(20, file='Ne13.dat')
+        read(20, *) (q0(1,i), q0(2,i), q0(3,i),i=1,natom)
+        close(20)
 
-	taumax = 1.0/20.0
-	call vgwquenchspb(natom,IMASS,q0,FX,LNP,W, ENRG,TAUMAX,TAUI,BL,ATOL,RC,Y, InvMeff,SqrtMeff)
-end
+        taumax = 1.0/20.0
+        call vgwquenchspb(natom,IMASS,q0,FX,LNP,W, ENRG,TAUMAX,TAUI,BL,ATOL,RC,Y, InvMeff,SqrtMeff)
+end program ljmc
