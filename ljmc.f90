d8f890ec0d68b0d986d5e738f84dfa35e8949812
Ionut Georgescu <ionut.georgescu@uci.edu>
2010-06-30 16:11:47 -0700

    Remove white spaces

--- ljmc.f90
+++ ljmc.f90
@@ -3,10 +3,10 @@ program ljmc
         real*8 :: q0(3,natom), qtau(3,natom), fx(3,natom), y(1+21*natom), invmeff(3,3,natom), sqrtmeff(3,3,natom)
         real*8, parameter :: imass = 2.0, taui = 1.0e-6, bl=40.0, atol=1.0e-4, rc=10.0
         real*8 :: lnp, w, enrg, taumax
-	real*8, parameter, dimension(4) :: &
-		LJA = (/1.03825, 0.597404, 0.1964765, 0.0666861 /), &
-		LJC = (/96609.488289873, 14584.6207550751, &
-			-365.460614956589, -19.5534697800036/)
+        real*8, parameter, dimension(4) :: &
+                LJA = (/1.03825, 0.597404, 0.1964765, 0.0666861 /), &
+                LJC = (/96609.488289873, 14584.6207550751, &
+                        -365.460614956589, -19.5534697800036/)
 
 
         open(20, file='Ne13.dat')
