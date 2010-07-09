2549007ee5e21348c5ff985a36a0ddbe00248bb4
Ionut GEORGESCU <ionut.georgescu@uci.edu>
2010-07-09 14:06:13 -0700

    Use atlas in linux

--- gcc.mk
+++ gcc.mk
@@ -1,12 +1,13 @@
-OPTFLAGS=-O2 -g
+OPTFLAGS=-O3
 DBGFLAGS=-O0 -ggdb -Wall
 
-CFLAGS=-std=c99 
+CFLAGS=
 FFLAGS=-fdefault-real-8 -fdefault-double-8 -ffree-line-length-0 -ffixed-line-length-0
 FDBG:=-fbounds-check -Wimplicit
 
 ifeq ($(OS),Darwin)
 	LAPACK:=-framework vecLib
 endif
+LAPACK=-lf77blas -lcblas -latlas
 
 
