fb4cb4a0e9e26419148a3ca80424fc1bec2282ed
Ionut Georgescu <ionut.georgescu@uci.edu>
2010-07-27 22:18:52 -0700

    use correct interfaces

--- propagation.f90
+++ propagation.f90
@@ -2,17 +2,26 @@ module propagation
 contains
 subroutine verlet(F, Q, G, gamma0, tstart, tstop, dt)
         use vgw
-      REAL*8, intent(inout) :: Q(3,N_atom), G(3,3,N_atom), gamma0
+        REAL*8, intent(inout) :: Q(3,N_atom), G(3,3,N_atom), gamma0
         real*8, intent(in) :: tstart, tstop, dt
-      REAL*8 :: Q1(3,N_atom), G1(3,3,N_atom)
-      REAL*8 :: QP(3,N_atom), GP(3,3,N_atom), gamma0p
+        REAL*8 :: Q1(3,N_atom), G1(3,3,N_atom)
+        REAL*8 :: QP(3,N_atom), GP(3,3,N_atom), gamma0p
+        interface
+        SUBROUTINE F(Q, BLKC, gamma0, Qprime, Gprime, gamma0prime)
+                use vgw
+              IMPLICIT NONE
+              REAL*8, intent(in) :: Q(3,N_atom), BLKC(3,3,N_atom), gamma0
+              REAL*8, intent(out) :: Qprime(3,N_atom), Gprime(3,3,N_atom), gamma0prime
+        end subroutine
+        end interface
+       
 
         nsteps = (tstop - tstart) / dt
         do i = 1, nsteps
                 call F(Q, G, gamma0, QP, GP, gamma0p)
                 Q1 = Q + 0.5*dt*QP
                 G1 = G + 0.5*dt*GP
-                call F(N_atom, Q1, G1, gamma0, QP, GP, gamma0p)
+                call F(Q1, G1, gamma0, QP, GP, gamma0p)
                 Q = Q + dt*QP
                 G = G + dt*GP
                 gamma0 = gamma0 + dt*gamma0p
