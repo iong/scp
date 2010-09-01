787b014f7d8575863eab04134937cb86cc56ed82
Ionut Georgescu <ionutg@gmail.com>
2010-08-31 23:16:26 -0700

    Forgot one more file

---
+++ dump_Tmin.f90
@@ -0,0 +1,18 @@
+subroutine dump_Tmin(MCiter)
+    use ljmc
+    use mc
+    implicit none
+    integer :: MCiter
+    integer :: i, j, im(1)
+    
+    write (*, "('n =',I10,', Umin = ',(F14.7$))") MCiter, Umin(1:nstreams)
+    write (*,*)
+
+
+    open(33, file=outfile)
+    write(33,'(I10)') Natom
+    write(33,"('Umin = ', ES14.7)") Umin(1)
+    write(33,"(('Ne ', 3(F12.5, ' ')))") ((rmin(i, j, 1), i=1,3), j=1,Natom)
+    close(33)
+end subroutine
+
