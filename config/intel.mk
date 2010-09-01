448283d1334b1e54ada6a3857267a20e4363cbf3
Ionut Georgescu <ionutg@gmail.com>
2010-08-31 23:11:12 -0700

    No OpenMP

--- config/intel.mk
+++ config/intel.mk
@@ -2,9 +2,9 @@ CC:=icc
 FC:=ifort
 OPTFLAGS=-O2
 DBGFLAGS=-O0 -g -warn unused
-FDBG:=-traceback -check all -ftrapuv
-FFLAGS:=-openmp
-LDFLAGS:=$(LDFLAGS) -openmp
+FDBG:=#-traceback -check all -ftrapuv
+FFLAGS:=
+LDFLAGS:=$(LDFLAGS)
 MKLLIBS:= mkl_intel_lp64 mkl_sequential mkl_core
 
 LAPACK:=-framework Intel_MKL
