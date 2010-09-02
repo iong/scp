1e9bad4e082f18185eeafb2075b0142bd0d8c5b1
Ionut Georgescu <ionut.georgescu@uci.edu>
2010-09-02 11:07:33 -0700

    Central dispatch

--- ljmc.f90
+++ ljmc.f90
@@ -3,6 +3,7 @@ program pljmc
     use mc
     use vgw
     implicit none
+    include 'mpif.h'
     real*8, parameter, dimension(4) :: &
             LJA = (/1.038252215127D0, 0.5974039109464D0, &
                     0.196476572277834D0, 0.06668611771781D0/), &
@@ -14,8 +15,12 @@ program pljmc
             
     real*8 :: rcmin
     character(LEN=256) :: arg, inputf
-    integer :: i, j, n, NMC,mcblen
-    namelist /ljmccfg/Natom,imass,rc,rtol,atol,taumin,NMC,mcblen,ptinterval,nstreams,Tmin,Tmax,bl,outfile,rho,rcmin,ncells
+    integer :: i, j, n, NMC,mcblen,ierr
+    namelist /ljmccfg/Natom,imass,rc,rtol,atol,taumin,NMC,mcblen,ptinterval,Tmin,Tmax,bl,outfile,rho,rcmin,ncells
+
+    call MPI_Init(ierr)
+    call MPI_Comm_rank(MPI_COMM_WORLD, me, ierr)
+    call MPI_Comm_size(MPI_COMM_WORLD, nprocs, ierr)
 
     if (command_argument_count() /= 1) then
         write (*,*) 'Need input file'
@@ -30,7 +35,7 @@ program pljmc
     open(20, file=inputf)
     read(20, NML=ljmccfg)
     close(20)
-
+    
     call setup_ljmc()
     call seed_rng()
 
@@ -49,26 +54,28 @@ program pljmc
 
     IMASS = IMASS*0.020614788876D0
     call vgwinit(natom, 3, pH2C, pH2A, bl)
-    do i=1,nstreams
-        call populate_cube(bl, rcmin, r(:,:,i))
-        call vgw0(r(:,:,i), U0(i), beta(i), 0.0d0, y0)
-    enddo
+    call populate_cube(bl, rcmin, r(:,:))
+    call vgw0(r(:,:), U0, beta(me), 0.0d0, y0)
 
     call mc_burnin(10000)
-    write (*,*) 'xstep', xstep(1,:)
+    write (*,*) 'xstep', xstep
 
 
     do n=1,NMC,mcblen
         call mc_block(mcblen, 1000)
-        write (*,*) 'xstep', xstep(1,:)
-
-        call dump_Tmin(n)
-        call heat_capacity(nstreams, Z, kT, Cv)
-        open(30, file='Z.dat')
-        write (30,'(4(ES16.8," "))') (kT(i), Cv(i), Z(i), beta(i),i=1,nstreams)
-        close(30)
+        write (*,*) 'xstep', xstep
+    
+        call MPI_Gather(Z(me), 1, MPI_REAL8, Z, nprocs, MPI_REAL8, 0, MPI_COMM_WORLD)
+        if (me==0) then
+            call dump_Tmin(n)
+            call heat_capacity(nprocs, Z, kT, Cv)
+            open(30, file='Z.dat')
+            write (30,'(4(ES16.8," "))') (kT(i), Cv(i), Z(i), beta(i),i=1,nprocs)
+            close(30)
+        endif
     enddo
 
     call dump_Tmin(NMC)
+    call MPI_Finalize(ierr)
 end program pljmc
 ! vim:et
--- mc.f90
+++ mc.f90
@@ -2,10 +2,10 @@ module mc
     use ljmc
     implicit none
     integer, parameter :: nmoves = 4
-    real*8, allocatable :: r(:,:,:), rnew(:,:,:), rmin(:,:,:), U0(:), Umin(:)
-    real*8, allocatable :: xstep(:,:), Z(:), kT(:), beta(:), Cv(:)
-    integer, allocatable :: naccepted(:,:), ntrials(:,:), tpool(:), stepdim(:)
-    integer :: nstreams, ptinterval
+    real*8, allocatable :: r(:,:), rnew(:,:), rmin(:,:), U0, Umin
+    real*8, allocatable :: xstep(:), Z(:), kT(:), beta(:), Cv(:)
+    integer, allocatable :: naccepted(:), ntrials(:), tpool(:), stepdim(:)
+    integer :: ptinterval
     real*8 :: Tmin, Tmax
 contains
 
@@ -75,99 +75,69 @@ end subroutine
 
 
 
-subroutine mc_trial(irep, istep)
+subroutine mc_trial(istep)
     use vgw
     implicit none
-    integer, intent(in) :: irep, istep
+    integer, intent(in) :: istep
     integer, parameter :: acceptance_trials = 100
     integer :: i, k, j
     real*8 :: lUmin, Unew, p, rn
 
-    rnew(:,:,irep) = r(:,:,irep)
+    rnew(:,:) = r(:,:)
     lUmin = 1d10
-    ntrials(irep,istep) = ntrials(irep,istep) + 1
+    ntrials(istep) = ntrials(istep) + 1
     call rnd_tuple2(stepdim(istep))
 
     do i=1,stepdim(istep)
         j = tpool(i)
-        call mc_move_atom(rnew(:,j,irep), xstep(irep, istep))
+        call mc_move_atom(rnew(:,j), xstep( istep))
 
         do k=1,3
-            if (rnew(k,j,irep) > bl2) then
-                rnew(k,j,irep) = rnew(k,j,irep) - bl
-            elseif (rnew(k,j,irep) < -bl2) then
-                rnew(k,j,irep) = rnew(k,j,irep) + bl
+            if (rnew(k,j) > bl2) then
+                rnew(k,j) = rnew(k,j) - bl
+            elseif (rnew(k,j) < -bl2) then
+                rnew(k,j) = rnew(k,j) + bl
             endif
         enddo
     enddo
 
-    call vgw0(rnew(:,:,irep), Unew, beta(irep), 0.0d0, y0)
-    p = exp( - (Unew - U0(irep)) * beta(irep) )
+    call vgw0(rnew(:,:), Unew, beta(me), 0.0d0, y0)
+    p = exp( - (Unew - U0) * beta(me) )
     call random_number(rn)
 
     if (p>rn) then
-        U0(irep) = Unew
-        r(:,:,irep) = rnew(:,:,irep)
-        naccepted(irep,istep) = naccepted(irep,istep) + 1
+        U0 = Unew
+        r(:,:) = rnew(:,:)
+        naccepted(istep) = naccepted(istep) + 1
     endif
 
-    if (U0(irep) < Umin(irep)) then
-        Umin(irep) = U0(irep)
-        rmin(:,:,irep) = r(:,:,irep)
+    if (U0 < Umin) then
+        Umin = U0
+        rmin(:,:) = r(:,:)
     endif
 
-    if (mod(ntrials(irep,istep), acceptance_trials) == 0) then
-        if (naccepted(irep,istep) < 0.3*acceptance_trials) then
-            xstep(irep,istep) = xstep(irep,istep) / 1.10779652734191
-        elseif (naccepted(irep,istep) > 0.4*acceptance_trials) then
-            xstep(irep,istep) = xstep(irep,istep) * 1.12799165273419;
+    if (mod(ntrials(istep), acceptance_trials) == 0) then
+        if (naccepted(istep) < 0.3*acceptance_trials) then
+            xstep(istep) = xstep(istep) / 1.10779652734191
+        elseif (naccepted(istep) > 0.4*acceptance_trials) then
+            xstep(istep) = xstep(istep) * 1.12799165273419;
         endif
-        naccepted(irep,istep) = 0
+        naccepted(istep) = 0
     endif
-end subroutine
-
-subroutine pt_swap()
-    use vgw
-    implicit none
-    real*8 :: rtmp(3,Natom), rn, p
-    real*8 :: U_betai_ri,U_betai_rj,U_betaj_ri,U_betaj_rj
-    integer :: i, j
-    call random_number(rn)
-    i = 1 + aint( rn * (nstreams-1) )
-    j = i+1
-
-    U_betai_ri = U0(i);
-    U_betaj_rj = U0(j);
-    call vgw0(r(:,:,i), U_betaj_ri, beta(j), 0.0d0, y0)
-    call vgw0(r(:,:,j), U_betai_rj, beta(i), 0.0d0, y0)
-
-    p = exp(-beta(i)*(U_betai_rj - U_betai_ri)-beta(j)*(U_betaj_ri - U_betaj_rj))
-    call random_number(rn)
-    if (p>rn) then
-        write (*,*) 'swapping', i,j, 'p=',p
-        rtmp(:,:) = r(:,:,i)
-        r(:,:,i) = r(:,:,j)
-        r(:,:,j) = rtmp(:,:)
-    endif
-    return
 end subroutine
 
 subroutine mc_block(blen, sublen)
     integer, intent(in) :: blen, sublen
-    integer :: irep, nsub, i, j, k, istep
+    integer :: nsub, i, j, istep
     real*8 :: rn
 
     nsub = blen/sublen
     do i=1,nsub
         call random_number(rn)
         istep = 1 + aint(nmoves*rn)
-        do j=1,sublen,ptinterval
-            do irep=1,nstreams
-                do k=1,ptinterval
-                    call mc_trial(irep, istep)
-                    Z(irep) = Z(irep) + exp(-beta(irep) * U0(irep))
-                enddo
-            enddo
+        do j=1,sublen
+            call mc_trial(istep)
+            Z(me) = Z(me) + exp(-beta(me) * U0)
             call pt_swap()
         enddo
     enddo
@@ -176,17 +146,80 @@ end subroutine
 
 subroutine mc_burnin(burnlen)
     integer, intent(in) :: burnlen
-    integer :: irep, j, k
+    integer ::  j, k
 
-    xstep(:,1) = 0.1
-    do irep=1,nstreams
-        do j=1,nmoves
-            if (j>1) xstep(irep, j) = xstep(irep, j-1)
-            do k=1,burnlen
-                call mc_trial(irep, j)
-            enddo
+    xstep(1) = 0.1
+    do j=1,nmoves
+        if (j>1) xstep(j) = xstep(j-1)
+        do k=1,burnlen
+            call mc_trial(j)
         enddo
     enddo
 end subroutine
+
+subroutine pt_swap(nmcsteps)
+    use vgw
+    implicit none
+    include 'mpif.h'
+    integer, intent(in) :: nmcsteps
+    real*8 :: rtmp(3,Natom), rn, p
+    real*8 :: U_rlow(3),U_rhigh(3)
+    integer :: ilow, ihigh, i, req(2), IERR, s(MPI_STATUS_SIZE), idx, flag
+    integer :: tag = 0, dest
+
+    if (me==0 .and. mod(nmcsteps,ptinterval)==0) then
+        call random_number(rn)
+        ilow = aint((nprocs-1) * rn)
+        call MPI_Isend(ilow, 1, MPI_INTEGER, ilow+1, tag, MPI_COMM_WORLD, req(2), IERR)
+        if (ilow > 0) then
+            call MPI_Isend(ilow, 1, MPI_INTEGER, ilow, tag, MPI_COMM_WORLD, req(1), IERR)
+            call MPI_Waitany(2, req, idx, s, IERR)
+            write (*,*) 'ack', idx
+            call MPI_Waitany(2, req, idx, s, IERR)
+            write (*,*) 'ack', idx
+            return
+        else
+            call MPI_Wait(req(2), s, IERR)
+        endif
+    else
+        call MPI_Iprobe(0, tag, MPI_COMM_WORLD, flag, s, IERR)
+        if (flag) then
+            call MPI_Recv(ilow,1,MPI_INTEGER, 0, tag, MPI_COMM_WORLD, ierr)
+            write (*,*) ilow
+        else
+            return
+        endif
+    
+    endif
+
+    ihigh = ilow+1
+
+    call random_number(rn)
+    if (me==ilow) then
+        U_rlow(1) = U0
+        call vgw0(r(:,:), U_rlow(2), beta(ihigh), 0.0d0, y0)
+        U_rlow(3) = rn
+        call MPI_Sendrecv(U_rlow, 3, MPI_REAL8, ihigh, tag, &
+                U_rhigh, 3, MPI_REAL8, ihigh, tag, MPI_COMM_WORLD, s, IERR)
+    else
+        call vgw0(r(:,:), U_rhigh(1), beta(ilow), 0.0d0, y0)
+        U_rhigh(2) = U0
+        U_rhigh(3) = 0
+        call MPI_Sendrecv(U_rhigh, 3, MPI_REAL8, ilow, tag, &
+                U_rlow, 3, MPI_REAL8, ilow, tag, MPI_COMM_WORLD, s, IERR)
+        rn = U_rlow(3)
+    endif
+    
+    p = exp(-beta(ilow)*(U_rhigh(1) - U_rlow(1))-beta(ihigh)*(U_rlow(2) - U_rhigh(2)))
+    if (p>rn) then
+        write (*,*) 'swapping', ilow,ihigh, 'p=',p
+        dest = ilow
+        if (me == ilow) dest=ihigh
+        call MPI_Sendrecv(r, 3*Natom, MPI_REAL8, dest, tag, &
+                rtmp, 3*Natom, MPI_REAL8, dest, tag, MPI_COMM_WORLD, s, IERR)
+    endif
+    return
+end subroutine
+
 
 end module mc
