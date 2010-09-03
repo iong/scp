652eb598cc6800d9285ef493c13423dd2ac1d468
Ionut Georgescu <ionut.georgescu@uci.edu>
2010-09-03 16:37:16 -0700

    Belongs to the previous commit

--- vgw.f90
+++ vgw.f90
@@ -1,6 +1,6 @@
 module vgw
-    real*8, dimension(10), private :: LJA, LJC
-    integer, private :: NGAUSS
+    real*8, dimension(10) :: LJA, LJC
+    integer :: NGAUSS
     real*8, private :: BL
     logical, allocatable :: QRC(:)
     real*8 :: MASS, RC, ATOL, RTOL, TAUMIN, IMASS
@@ -8,14 +8,11 @@ module vgw
     
 contains
 
-subroutine vgwinit(natom, ng, c, a, boxlen)
+subroutine vgwinit(natom, boxlen)
     implicit none
-    integer, intent(in) :: natom, ng
-    real*8, intent(in) :: boxlen, c(ng), a(ng)
+    integer, intent(in) :: natom
+    real*8, intent(in) :: boxlen
     N_atom = natom
-    NGAUSS = ng
-    LJC(1:ng) = c(1:ng)
-    LJA(1:ng) = a(1:ng)
     BL = boxlen
     MASS=1.0D0/IMASS
 
