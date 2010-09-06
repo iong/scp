ce4bc7befa4bb6bd4d51d6171436d3ad97defe15
Ionut Georgescu <ionut.georgescu@uci.edu>
2010-09-06 16:09:06 -0700

    debugging

--- ljmc.f90
+++ ljmc.f90
@@ -2,11 +2,13 @@ program pljmc
     use ljmc
     use mc
     use vgw
+    use xyz
     implicit none
     include 'mpif.h'
     real*8 :: rcmin
-    character(LEN=256) :: arg, inputf
+    character(LEN=256) :: arg, inputf, fname
     integer :: i, j, n, NMC,mcblen,ierr
+    character(4) :: csubme
     namelist /ljmccfg/Natom,imass,NGAUSS,LJA,LJC,rc,rtol,atol,taumin,NMC,mcblen,ptinterval,Tmin,Tmax,outfile,rho,rcmin
 
     call MPI_Init(ierr)
@@ -46,13 +48,20 @@ program pljmc
 
     IMASS = IMASS*0.020614788876D0
     call vgwinit(natom, bl)
-    call populate_cube(bl, rcmin, r(:,:))
-    U0=0
+
+    !call populate_cube(bl, rcmin, r(:,:))
+
+    call int2str(me, csubme)
+    fname = "r0."//csubme//".xyz"
+    call load_xyz(r, fname)
     call vgw0(r(:,:), U0, beta(me+1), 0.0d0, y0)
 
-    call mc_burnin(10)
-    write (*,*) 'xstep', xstep
-
+    !call mc_burnin(10000)
+    !write (*,*) 'xstep', xstep
+    
+    !call int2str(me, csubme)
+    !fname = "r0."//csubme//".xyz"
+    !call dump_xyz(r, fname, "(pH2)_50")
 
     Z = 0.0d0
     do n=1,NMC,mcblen
@@ -71,5 +80,15 @@ program pljmc
 
     call dump_Tmin(NMC)
     call MPI_Finalize(ierr)
+contains
+
+subroutine int2str(i, csubme)
+    integer, intent(in) :: i
+    character(4) :: csubme
+    csubme =   achar(i/1000+48) &
+           // achar(mod(i/100,10)+48) &
+           // achar(mod(i/10,10)+48) &
+           // achar(mod(i,10)+48)
+end subroutine
 end program pljmc
 ! vim:et
