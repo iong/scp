71cf218bcfc261703162ba1abee5f8e3b4bc0278
Ionut Georgescu <ionut.georgescu@uci.edu>
2010-09-28 18:00:31 -0700

    Unused variables

--- xyz.f90
+++ xyz.f90
@@ -4,7 +4,7 @@ subroutine dump_xyz(r, fname, molname)
     implicit none
     real*8, intent(in) :: r(:,:)
     character(len=*), intent(in) :: fname, molname
-    integer :: i, j, N
+    integer :: j, N
 
     N = size(r, 2)
     open(33,file=fname,status='REPLACE')
@@ -21,7 +21,7 @@ subroutine load_xyz(r, fname)
     real*8, intent(out) :: r(:,:)
     character(256), intent(in) :: fname
     character(256):: molname
-    integer :: i, j, N
+    integer :: j, N
 
     open(33,file=fname)
     read(33,*) N
