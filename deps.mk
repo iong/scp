3af0a05a727c59506801e0af3ee37a38012f1edf
Ionut GEORGESCU <ionut.georgescu@uci.edu>
2010-10-21 12:41:41 -0700

    mergecvv does not depend on vgw anymore

--- deps.mk
+++ deps.mk
@@ -5,8 +5,8 @@ xyz.mod: xyz.o
 spine.mod: spine.o
 spine.o: utils.mod vgw.mod
 gmd.o: propagation.mod spine.mod utils.mod vgw.mod xyz.mod
-xyz.mod: xyz.o
-mergecvv.o: utils.mod vgw.mod xyz.mod
+utils.mod: utils.o
+mergecvv.o: utils.mod
 xyz.mod: xyz.o
 utils.mod: utils.o
 lj.o: utils.mod xyz.mod
