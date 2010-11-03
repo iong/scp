9502922679d1cf75703b93615bc433bea4775f53
Ionut Georgescu <ionutg@gmail.com>
2010-11-02 22:58:36 -0700

    Trap FPEs

--- config/gcc.mk
+++ config/gcc.mk
@@ -5,7 +5,7 @@ DBGFLAGS=-O0 -ggdb -Wall
 
 CFLAGS=
 FFLAGS=-fdefault-real-8 -fdefault-double-8 -ffree-line-length-0 -ffixed-line-length-0 -fimplicit-none
-FDBG:=-fbounds-check 
+FDBG:=-fbounds-check -ffpe-trap=invalid,zero,overflow,denormal
 
 LAPACK=-lf77blas -lcblas -latlas
 
