04a2f24331ca7ae083028e53eaedb24b80d92301
Ionut GEORGESCU <ionut.georgescu@uci.edu>
2010-11-03 10:52:16 -0700

    Normalization, debug

--- kubo.f90
+++ kubo.f90
@@ -9,8 +9,8 @@ subroutine kubo(q0, v0, beta, nsteps, xk, vk)
     real*8, dimension(3, Natom) :: ql, qr, dxk, dvk
     real*8, dimension(3, 3, Natom) :: gl, gr, Qnkl, Qnkr
     real*8, dimension(1+21*Natom, 0:nsteps) :: yl
-    real*8 :: tau, dtau, Ueff, dqlr(3), invglr(3,3), detglr, qGq, &
-            vnkl(3), vnkr(3), detj, Ueffbeta, w
+    real*8 :: tau, dtau, Ueff, dqlr(3), invglr(3,3), logdet, qGq, &
+            vnkl(3), vnkr(3), detj, Ueffbeta, w, qlqr, qlqrint
     integer :: i, j
  
     if (mod(nsteps,2) /= 0) then
@@ -32,7 +32,8 @@ subroutine kubo(q0, v0, beta, nsteps, xk, vk)
 
     xk = 0.0d0
     vk = 0.0d0
-    do i=0,nsteps/2-1
+    qlqrint = 0.0d0
+    do i=0,nsteps
         call unpack_q(yl(:,i), ql)
         call unpack_g(yl(:,i), gl)
         call unpack_q(yl(:,nsteps-i), qr)
@@ -40,31 +41,34 @@ subroutine kubo(q0, v0, beta, nsteps, xk, vk)
         if (i==0) then
             w = 1.0d0
         else
-            w = 2.0d0
+            w = 1.0d0
         end if
 
         qGq = 0.0d0
-        detglr = 1.0d0
+        logdet = 0.0d0
         do j=1,Natom
             dqlr = ql(:,j) - qr(:,j)
             call detminvm(gl(:,:,j) + gr(:,:,j), detj, invglr)
             qGq = qGq + dot_product(dqlr, matmul(invglr, dqlr))
-            detglr = detglr * detj
+            logdet = logdet + log(detj)
             dxk(:,j) = matmul(invglr, matmul(gr(:,:,j), ql(:,j)) + matmul(gl(:,:,j), qr(:,j)))
             vnkl = matmul(Qnkl(:,:,j), v0(:,j))
             vnkr = matmul(Qnkr(:,:,j), v0(:,j))
             dvk(:,j) = matmul(invglr, matmul(gr(:,:,j), vnkl) + matmul(gl(:,:,j), vnkr))
             
         end do
-        xk = xk + dxk * exp(yl(1, i) + yl(1, nsteps-i) - 0.5*qGq)/sqrt(detglr)
-        vk = vk + dvk * exp(yl(1, i) + yl(1, nsteps-i) - 0.5*qGq)/sqrt(detglr)
-
+        qlqr = exp(yl(1, i) + yl(1, nsteps-i) - 0.5*(qGq + logdet))
+        qlqrint = qlqrint + w*qlqr
+        xk = xk + dxk * qlqr
+        vk = vk + dvk * qlqr
+        write (*,*) qlqr * exp(beta*Ueffbeta + 1.5d0*Natom*log(2.0d0))
     end do
+    write (*,*) 'qlqrint =',qlqrint/nsteps * exp(beta*Ueffbeta + 1.5d0*Natom*log(2.0d0))
 
     call unpack_q(yl(:,nsteps/2), ql)
     call unpack_qnk(yl(:,nsteps/2), Qnkl)
     do j=1,Natom
-        dvk(:,j) = matmul(Qnkl(:,:,j), v(:,j))
+        dvk(:,j) = matmul(Qnkl(:,:,j), v0(:,j))
     end do
     
     xk = (exp(beta*Ueffbeta) * xk + ql) / nsteps
