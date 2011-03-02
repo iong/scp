980337536caa5b76946aa80f18f73e86c57742e5
Ionut Georgescu <ionutg@gmail.com>
2011-03-01 21:30:18 -0800

    Put automatic arrays on the heap

--- config/intel.mk
+++ config/intel.mk
@@ -3,10 +3,9 @@ FC:=ifort
 OPTFLAGS=-fast
 DBGFLAGS=-O0 -g -warn unused
 FDBG:=-fpe0 #-traceback -check all -ftrapuv
-FFLAGS:=
-LDFLAGS:=$(LDFLAGS)
+FFLAGS:=-heap-arrays
+LDFLAGS:=$(LDFLAGS) -heap-arrays
 MKLLIBS:= mkl_intel_lp64 mkl_sequential mkl_core
 
-LAPACK:=-framework Intel_MKL
-LAPACK:=-L$(MKLROOT)/lib/em64t $(addprefix -l,$(MKLLIBS))
+LAPACK:=-mkl=sequential
 
