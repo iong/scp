ba7d2fc0c61e93cf9ae4540f3745bba0d7e989fb
Ionut Georgescu <ionut.georgescu@uci.edu>
2012-01-05 12:02:36 -0800

    Don't warn for temporaries; OSX Lion idb fix

--- config/intel.mk
+++ config/intel.mk
@@ -2,10 +2,13 @@ CC:=icc
 FC:=ifort
 OPTFLAGS=-fast
 DBGFLAGS=-O0 -g -warn unused
-FDBG:=-fpe0 -traceback -check all -ftrapuv
-FFLAGS:=-heap-arrays
-LDFLAGS:=$(LDFLAGS) -heap-arrays
+FDBG:=-fpe0 -traceback -check all -check noarg_temp_created -ftrapuv
+FFLAGS:=
 MKLLIBS:= mkl_intel_lp64 mkl_sequential mkl_core
 
 LAPACK:=-mkl=sequential
 
+OS_RELEASE=$(shell uname -r | sed 's/\..*$$//')
+ifeq ($(OS_RELEASE), 11)
+	LDFLAGS += -Wl,-no_pie
+endif
