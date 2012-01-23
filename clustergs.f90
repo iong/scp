b08d288cce13e9d65f735554a3335ff8789c8114
Ionut Georgescu <ionut.georgescu@uci.edu>
2012-01-23 10:17:46 -0800

    Remove clutter.

--- clustergs.f90
+++ clustergs.f90
@@ -10,10 +10,10 @@ program clustergs
     integer :: i, Natom, status
 
     double precision :: endtime(3), begtime(3), mass, taustop
-    double precision ::  deBoer=0.1, kT=0.0001d0, RC=100d0, BL=100d0, sigma0 = 2.749, epsilon0 = 35.6d0
+    double precision ::  deBoer=0.1, kT=0.001d0, RC=100d0, BL=100d0, sigma0 = 2.749, epsilon0 = 35.6d0
 
     double precision, allocatable :: r0(:,:)
-    double precision :: U(4), Uinf(4), rt(3)
+    double precision :: Uref, E0
 
     class(vgwfm), pointer :: fm
 
@@ -24,13 +24,12 @@ program clustergs
     open(33, file=trim(coords))
     read (33, *) Natom
 
-
     allocate(r0(3,Natom))
-    U = 0
 
-    read(33,IOSTAT=status,FMT=*) deBoer, U(1)
+    Uref = 0d0
+    read(33,IOSTAT=status,FMT=*) deBoer, Uref
     if (status > 0) then
-        U(1) = 0d0
+        Uref = 0d0
     else if (status<0) then
         write (*,*) 'read(33,*) failed with status', status
         stop
@@ -39,6 +38,7 @@ program clustergs
     read(33, *) (waste, r0(:,i), i=1,Natom)
     close(33)
 
+    ! deBoer can be supplied as a second argument on the command line
     if (command_argument_count() > 1) then
         call get_command_argument(2, arg)
         read(arg, *) deBoer
@@ -52,12 +52,12 @@ program clustergs
     call fm%init(Natom, 'LJ')
     call fm%set_mm(.TRUE.)
     call fm%set_mass((/ ( mass, i=1,Natom ) /) )
-    U(3) = fm%Havg(r0, taustop)
+    E0 = fm%Havg(r0, taustop)
     call fm%Feff(r0)
-    write (*,*) r0
     call fm%cleanup()
 
-    U(2:4) = U(2:4) * epsilon0
+    !U(2:4) = U(2:4) * epsilon0
 
-    write(*,*) U
+    write(*,'(I10,2F12.7)') Natom, E0/Natom, sqrt(sum(r0**2)/(3*Natom))
+
 end program clustergs
