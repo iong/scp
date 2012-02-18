1748732baa21fd5248fc8a694062742bb3ddac95
Ionut Georgescu <ionut.georgescu@uci.edu>
2012-02-17 17:28:05 -0800

    Define the type, set_range() and cleanup().

--- ffvgw.f90
+++ ffvgw.f90
@@ -1,49 +1,51 @@
-module vgwspfm
+module ffvgw_mod
     use integration
     use utils
     use sparse
+    use vgw_mod
     implicit none
     private
-    public :: vgwspfminit,vgw0spfm,vgw0spfmgs,vgwspfmcleanup
+
+    type, public, extends(vgw) :: ffvgw
+	double precision :: rfullmatsq
+        double precision, allocatable :: UX(:)
+
+        double precision :: Vcutoff, Gcutoff
+
+        type(csr), target :: G, UXY, GU
+        type csr_list
+            type(csr), pointer :: p
+        end type csr_list
+
+        double precision, allocatable :: Gbdiag(:,:), Gband(:,:), UXYbdiag(:,:), UXYband(:,:), UX(:)
+        integer, allocatable :: Gband_ja(:)
+    contains
+	procedure :: cleanup
+        procedure :: converge
+        procedure :: set_range
+	procedure :: init_prop
+	procedure :: logdet
+end type ffvgw
     
-    integer, parameter :: info = 1
-    integer :: debug = 0
-    integer :: Natom, NGAUSS, NEQ
-    real*8 :: BL, Vcutoff, Gcutoff, LAMBDA, sigma0, epsilon0
-    real*8, dimension(10) :: LJA, LJC
-
-    type(csr), target :: G, UXY, GU
-    type csr_list
-        type(csr), pointer :: p
-    end type csr_list
-
-    real*8 :: gama, gamap, U
-    real*8, allocatable :: Gbdiag(:,:), Gband(:,:), UXYbdiag(:,:), UXYband(:,:), UX(:)
-    integer, allocatable :: Gband_ja(:)
-
-
-    double precision, allocatable, target :: Y(:)
-    class(integrator), pointer :: prop
-
-    real*8 :: invmass, mass, dt0, dtmax, dtmin, vgw_atol(3)
-
 contains
 
-subroutine vgwspfminit(species, M, Vrange, Grange)
-    implicit none
-    character(*), intent(in) :: species
-    real*8, intent(in), optional :: M, Vrange, Grange
+    subroutine set_range(self, Vcutoff, Gcutoff)
+        implicit none
+        class(ffvgw) :: self
+        double precision :: intent(in) :: Vrange, Grange
 
-    include 'species.f90'
-end subroutine
+        self % Vcutoff = Vcutoff
+        self % Gcutoff = Gcutoff
+    end subroutine set_range
 
-subroutine vgwspfmcleanup()
-     call G%cleanup()
-     call UXY%cleanup()
-     call GU%cleanup()
-     call prop % cleanup()
-     deallocate(Gbdiag, UXYbdiag, UX, y)
- end subroutine vgwspfmcleanup
+    subroutine cleanup(self)
+        class(ffvgw) :: self
+        call self % G   % cleanup()
+        call self % UXY % cleanup()
+        call self % GU  % cleanup()
+
+        deallocate(Gbdiag, UXYbdiag, UX, y)
+    end subroutine cleanup
 
 include 'vgw0spfm.f90'
 
