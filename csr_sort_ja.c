c897d5c50fc257d1cb57e9f26d2a3cb4435463a0
Ionut Georgescu <ionut@bolivar.(none)>
2012-03-01 17:47:59 -0800

    Reindent.

--- csr_sort_ja.c
+++ csr_sort_ja.c
@@ -2,15 +2,15 @@
 
 static int int_cmp(const void *a, const void *b)
 {
-  return *((int *)a) - *((int *)b);
+    return *((int *)a) - *((int *)b);
 }
 
 
 void csr_sort_ja(int m, int *ia, int *ja)
 {
-  int     i;
+    int     i;
 
-  for (i=0; i<m; i++) {
-    qsort(ja + ia[i] - 1, ia[i+1] - ia[i], sizeof(int), &int_cmp);
-  }
+    for (i=0; i<m; i++) {
+        qsort(ja + ia[i] - 1, ia[i+1] - ia[i], sizeof(int), &int_cmp);
+    }
 }
