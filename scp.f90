8c91897161e9faa68f433fef6d93d45a0ab97db9
Ionut Georgescu <ionut.georgescu@uci.edu>
2012-03-05 15:44:37 -0800

    Rename the main program to scp

--- scp.f90
+++ scp.f90
@@ -1,4 +1,4 @@
-program clustergs
+program scp
 !    use vgw
     use vgw_mod
     use ffvgw_mod
@@ -17,22 +17,23 @@ program clustergs
     double precision, allocatable :: r0(:,:)
     double precision :: Uref, E0
 
-    class(vgw), pointer :: scp
+    class(vgw), pointer :: p
 
     nargs = command_argument_count()
     narg = 1
 
     call get_command_argument(narg, arg)
-    if (arg == '-sparse') then
-        allocate(ffvgw::scp)
+    if (trim(arg) == '-sparse') then
+        allocate(ffvgw::p)
         narg = narg + 1
     else
-        allocate(vgwfm::scp)
+        allocate(vgwfm::p)
     end if
         
     call get_command_argument(narg, arg)
     call load_xyz(arg, r0)
     narg = narg + 1
+    Natom = size(r0, 2)
 
     if (narg <= nargs) then
         call get_command_argument(narg, arg)
@@ -46,24 +47,24 @@ program clustergs
         narg = narg + 1
     end if
 
-    call scp%init(Natom, 'LJ')
+    call p%init(Natom, 'LJ')
     ! set the cutoffs if the sparse approach is used
-    select type (scp)
+    select type (p)
     type is(ffvgw)
-        call scp%set_range(1.8d0, 2.75d0)
+        call p%set_range(1.8d0, 2.75d0)
     end select
-    E0 = scp%F(reshape(r0, (/3*Natom/)) , kT)
+    E0 = p%F(reshape(r0, (/3*Natom/)) , kT)
 
     !ff % Omega % x => ff%y(3*Natom+1 : 3*Natom + ff%Omega%nnz)
     !call ff % Omega % write('Omega.csr')
     !nullify(ff % Omega % x)
 
     do
-        print '(F6.3,2F12.7,2E12.5)', kT, E0, scp % qconv, scp % gconv
+        print '(F6.3,2F12.7,2E12.5)', kT, E0, p % qconv, p % gconv
         kT = kT + 0.01
         if (kT >  kTstop) exit
-        E0 = scp%F(reshape(r0, (/3*Natom/)) , kT, .TRUE.)
+        E0 = p%F(reshape(r0, (/3*Natom/)) , kT, .TRUE.)
     end do
-    call scp%cleanup()
+    call p%cleanup()
 
-end program clustergs
+end program scp
