9b2b6266e8238c281e9cb9cbd29dfd75ee96bcd8
Ionut Georgescu <ionut.georgescu@uci.edu>
2012-03-12 10:09:38 -0700

    find_library_list() is needed in several macros.

---
+++ FindLibraryList.cmake
@@ -0,0 +1,19 @@
+macro(Find_Library_List LIBRARIES _list)
+  if (WIN32)
+    set(_libdir ENV LIB)
+  elseif (APPLE)
+    set(_libdir ENV DYLD_LIBRARY_PATH)
+  else ()
+    set(_libdir  ENV LD_LIBRARY_PATH)
+  endif ()
+  
+  set(_libdir HINTS ${ARGN} ${_libdir})
+
+  message("xxxxx ${_libdir}")
+  foreach(_lib ${_list})
+    find_library(${_lib}_LIBRARY NAMES ${_lib} ${_libdir})
+    set(${LIBRARIES} ${${LIBRARIES}} ${${_lib}_LIBRARY})
+  endforeach()
+endmacro()
+
+
