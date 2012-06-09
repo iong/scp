6bd8e8764e609a4dd3ec50ebafc8479015287d96
Ionut Georgescu <ionut.georgescu@uci.edu>
2012-06-08 17:00:59 -0700

    Real Hessian with the real Lennard-Jones potential

--- hsm.f90
+++ hsm.f90
@@ -8,7 +8,8 @@ program scp
 
     integer :: i, Natom
 
-    double precision, allocatable :: r0(:,:), H(:,:)
+    double precision :: U0
+    double precision, allocatable :: r0(:,:), H(:,:), W(:)
 
 
     call get_command_argument(1, fname)
@@ -16,19 +17,22 @@ program scp
        
     Natom = size(r0, 2)
 
-    allocate(H(3*Natom, 3*Natom))
+    allocate(H(3*Natom, 3*Natom), W(3*Natom))
 
-    H = hessian(r0)
+    call hessian(r0, U0, H)
+    W = getev(H)
+    deallocate(r0, H)
 
     fname = fname(1:index(fname,'.xyz')-1) // '_spectr.dat'
-    call printev(H, fname)
- 
-    deallocate(r0, H)
+    open(35, file=trim(fname))
+    write(35,'(F18.10)') W
+    close(35)
+    print *, Natom, U0/Natom, sum(W(7:))/(3*Natom - 6)
 contains
-    subroutine printev(A, fname)
-        double precision :: A(:,:)
-        character(*) :: fname
-        double precision, allocatable :: work(:), W(:), U(:,:)
+    function getev(A) result(W)
+        double precision, intent(in) :: A(:,:)
+        double precision :: W(size(A, 1))
+        double precision, allocatable :: work(:), U(:,:)
         integer,allocatable :: iwork(:),isuppz(:)
 
         integer :: N, info, NEV
@@ -41,19 +45,17 @@ contains
         N = size(A, 1)
 
         allocate(U(N,N))
-        allocate(work(N * 26), W(N), iwork(N * 10), isuppz(2*N))
+        allocate(work(N * 26), iwork(N * 10), isuppz(2*N))
         call dsyevr('V', 'A', 'L', N, A, N, 0d0, 0d0, 1, 6, &
                 DLAMCH('Safe minimum'), NEV, W, U, N, isuppz, &
                 work, size(work), iwork, size(iwork), info)
     
-        open(35, file=trim(fname))
-        write(35,'(F18.10)') W
-        close(35)
 
-        deallocate(W,work,iwork,isuppz, U)
-    end subroutine printev
 
-    function hessian(q) result(UXY)
+        deallocate(work,iwork,isuppz, U)
+    end function getev
+
+    function ghessian(q) result(UXY)
         !    use omp_lib
         IMPLICIT NONE
         double precision, intent(in) :: q(:,:)
@@ -93,5 +95,46 @@ contains
                 UXY(I2 : I2+2, I1 : I1+2) = -UXY0
             end do ! I2
         end do ! I1
-    END function hessian
+    END function ghessian
+
+    subroutine hessian(q, Utot, UXY)
+        !    use omp_lib
+        IMPLICIT NONE
+        double precision, intent(in) :: q(:,:)
+        double precision,intent(out) :: Utot, UXY(size(q), size(q))
+
+        double precision  :: U12, Q12(3), UXY0(3,3), qsq, y8, y6, A, B        
+        integer :: J,I1,I2
+
+        Utot = 0d0
+        UXY = 0d0
+        do I1=1,3*size(q,2),3
+            DO I2=I1+3,3*size(q,2),3
+                Q12 = q(:,(I1+2)/3) - q(:,(I2+2)/3)
+
+                qsq = sum(q12**2)
+                y8 = 1d0/qsq**4
+
+                y6 = y8 * qsq
+
+                Utot = Utot + 4d0*y6*(y6 - 1d0)
+
+                A = (672d0*y8 - 192d0/qsq)*y8;
+                do J=1,3
+                    UXY0(:,J) = (A*Q12(J))*Q12
+                end do
+
+                B = 24d0*y8 - 48d0*y8*y8*qsq
+                do J=1,3
+                    UXY0(J,J) = UXY0(J,J) + B
+                end do
+                
+                UXY(I1 : I1+2, I1 : I1+2) = UXY(I1 : I1+2, I1 : I1+2) + UXY0
+                UXY(I2 : I2+2, I2 : I2+2) = UXY(I2 : I2+2, I2 : I2+2) + UXY0
+
+                UXY(I1 : I1+2, I2 : I2+2) = -UXY0
+                UXY(I2 : I2+2, I1 : I1+2) = -UXY0
+            end do ! I2
+        end do ! I1
+    END subroutine hessian
 end program scp
